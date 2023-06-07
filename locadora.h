1bf4774d8066e97d80a95b176630526c8d076543
att4ck3rs3cur1ty <att4ck3rs3cur1ty>
2023-06-07 19:52:52 -0300

    Adding locadora.h

---
+++ locadora.h
@@ -0,0 +1,52 @@
+#include <iostream>
+
+class Pessoa {
+
+};
+class Cliente : Pessoa{
+
+};
+
+class Veiculo{
+
+};
+
+class CategoriaVeiculo : Veiculo{
+
+};
+
+class Reserva{
+
+};
+
+class Filial{
+
+};
+
+class Funcionario : Pessoa{
+
+};
+
+class AgenteAtendimento : Pessoa{
+
+};
+
+class Seguro{
+
+};
+
+class Pagamento{
+
+};
+
+class Manutencao{
+
+};
+
+class Promocao{
+
+};
+
+class HistoricoAlguel : Cliente{
+
+};
