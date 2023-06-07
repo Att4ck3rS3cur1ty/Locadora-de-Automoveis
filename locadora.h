17b3c10fd2362a8c27365b395fb51eb6a15e1f6e
att4ck3rs3cur1ty <att4ck3rs3cur1ty>
2023-06-07 20:46:19 -0300

    Adding Veiculo attributes, getters and setters

--- locadora.h
+++ locadora.h
@@ -9,6 +9,7 @@ class Pessoa{
         std::string numero_telefone;
     public:
         Pessoa(std::string data_nasc, std::string nome);
+
         std::string getDataNascimento();
         std::string setDataNascimento(std::string d);
 
@@ -29,7 +30,45 @@ class Cliente : Pessoa{
 };
 
 class Veiculo{
+    private:
+        int id;
+        std::string modelo;
+        std::string marca;
+        int ano;
+        std::string placa;
+        std::string cor;
+        float quilometragem;
+        int capacidade_passageiros;
+        std::string tipo_combustivel;
+    public:
+        Veiculo();
+        Veiculo(int id, std::string modelo, std::string marca, 
+        int ano, std::string placa, std::string cor, float quilometragem,
+        int capacidade_passageiros, std::string tipo_combustivel);
 
+        int getId();
+        int setId(int i);
+
+        std::string getModelo();
+        std::string setModelo(std::string m);
+
+        int getAno();
+        int setAno(int a);
+
+        std::string getPlaca();
+        std::string setPlaca(std::string p);
+
+        std::string getCor();
+        std::string setCor(std::string c);
+
+        float getQuilometragem();
+        float setQuilometragem(float q);
+
+        int getCapacidadePassageiros();
+        int setCapacidadePassageiros(int c);
+
+        std::string getTipoCombustivel();
+        std::string setTipoCombustivel(std::string t);
 };
 
 class CategoriaVeiculo : Veiculo{
