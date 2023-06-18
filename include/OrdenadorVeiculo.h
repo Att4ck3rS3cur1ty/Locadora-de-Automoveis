2e7b881ea34394f63515ec8a154b2cb6d4b8d5da
Cleiton Lima <cleiton.lima.105@ufrn.edu.br>
2023-06-18 20:39:27 -0300

    feat: Adição dos algoritimos de ordenação 'bubbleSorte' e 'quickSort'

---
+++ include/OrdenadorVeiculo.h
@@ -0,0 +1,17 @@
+// OrdenadorVeiculo.h
+
+#ifndef ORDENADOR_H
+#define ORDENADOR_H
+
+#include <vector>
+#include "Veiculo.h"
+
+// Algoritimos de ordenação que serão utilizados para ordenar os veículos
+
+class OrdenadorVeiculo {
+public:
+    static void bubbleSort(std::vector<Veiculo>& vetor);
+    static void quickSort(std::vector<Veiculo>& vetor, int esquerda, int direita);
+};
+
+#endif
