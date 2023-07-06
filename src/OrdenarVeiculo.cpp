3f69e372bc81c7500de7c8c899fae13d8bd8f1e1
att4ck3rs3cur1ty <att4ck3rs3cur1ty>
2023-07-06 19:22:35 -0300

    adding bubble sort algorithm implementation to order Veiculos by getAno()

---
+++ src/OrdenarVeiculo.cpp
@@ -0,0 +1,22 @@
+#include "OrdenadorVeiculo.h"
+
+void OrdenadorVeiculo::bubbleSort(std::vector<Veiculo>& vetor){
+    for (int i = vetor.size() - 1; i > 0; i--)
+    {
+        bool permutado = false;
+        for (int j = 0; j < i; j++)
+        {
+            if (vetor[j].getAno() > vetor[j+1].getAno())
+            {
+                (vetor[j], vetor[j+1]) = (vetor[j+1], vetor[j]);
+                permutado = true;
+            }
+        }
+
+        if(!permutado){
+            break;
+        }
+        
+    }
+    
+}
