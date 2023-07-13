439db3bd4c07c49fad356b856db99b9cfb3f564d
att4ck3rs3cur1ty <att4ck3rs3cur1ty>
2023-07-13 20:08:32 -0300

    adding salvarDados and carregarDados methods from Funcionario

--- src/Funcionario.cpp
+++ src/Funcionario.cpp
@@ -1,4 +1,7 @@
 #include "Funcionario.h"
+#include <iostream>
+#include <fstream>
+#include <string>
 
 Funcionario::Funcionario(const std::string& nome, const std::string& cpf, const std::string& endereco,
                          const std::string& telefone, const std::string& email, const std::string& cargo,
@@ -23,4 +26,88 @@ float Funcionario::getSalario() const {
 
 void Funcionario::setSalario(float salario) {
     this->salario = salario;
+}
+
+void salvarDados(const std::string& caminho_arquivo){
+    std::ofstream arquivo_stream(caminho_arquivo);
+    bool stop_reading = false;
+
+    arquivo_stream.open(caminho_arquivo);
+
+     if(arquivo_stream.fail()){
+        std::cerr << "Erro ao abrir o arquivo " << caminho_arquivo << std::endl;
+        return;
+    }
+
+    std::cout << "Inserindo dados..." << std::endl;
+    Funcionario* funcionario;
+    std::string str_aux;
+    float flt_aux;
+    char chr_aux;
+    while (!stop_reading)
+    {
+        std::cout << "========= Funcionário =========" << std::endl;
+        std::cout << "Nome: " << std::endl;
+        std::cin >> str_aux;
+        funcionario->setNome(str_aux);
+        arquivo_stream << funcionario->getNome() << ",";
+
+        std::cout << "CPF: " << std::endl;
+        std::cin >> str_aux;
+        funcionario->setCPF(str_aux);
+        arquivo_stream << funcionario->getCpf() << ",";
+    
+        std::cout << "Endereço: " << std::endl;
+        std::cin >> str_aux;
+        funcionario->setEndereco(str_aux);
+        arquivo_stream << funcionario->getEndereco() << ",";
+
+        std::cout << "Telefone: " << std::endl;
+        std::cin >> str_aux;
+        funcionario->setTelefone(str_aux);
+        arquivo_stream << funcionario->getTelefone() << ",";
+
+        std::cout << "Email: " << std::endl;
+        std::cin >> str_aux;
+        funcionario->setEmail(str_aux);
+        arquivo_stream << funcionario->getEmail() << ",";  
+
+        std::cout << "Cargo: " << std::endl;
+        std::cin >> str_aux;
+        funcionario->setCargo(str_aux);
+        arquivo_stream << funcionario->getCargo() << ",";
+
+        std::cout << "Salario: " << std::endl;
+        std::cin >> flt_aux;
+        funcionario->setSalario(flt_aux);
+        arquivo_stream << funcionario->getSalario();
+
+        std::cout << "Deseja adicionar mais um funcionário? S ou N" << std::endl;
+        std::cin >> chr_aux;
+
+        if (chr_aux == 'N' || chr_aux == 'n')
+        {
+            stop_reading = true;
+        }
+    }
+}
+
+void Funcionario::carregarDados(const std::string& caminho_arquivo) {
+    std::ifstream arquivo_stream(caminho_arquivo);
+
+    arquivo_stream.open(caminho_arquivo);
+
+     if(arquivo_stream.fail()){
+        std::cerr << "Erro ao abrir o arquivo " << caminho_arquivo << std::endl;
+        return;
+    }
+
+    while (arquivo_stream.peek()!=EOF)
+    {
+        std::string dados;
+        getline(arquivo_stream, dados, ',');
+        std::cout << dados << std::endl;
+    }
+
+    arquivo_stream.close();
 }
